40766168914daa1015f2f6a63e77d2bf55498a55
Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>
2024-01-18 01:34:49 -0600

    Signed-off-by: Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+const HatsPage = () => (
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+);
+
+function App() { // exact without = means true i.e. exact <=> exact={true}
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />  
+        <Route exact path='/hats' component={HatsPage} />  
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode> // With these tags navigation didn't work
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  // </React.StrictMode>
 );
