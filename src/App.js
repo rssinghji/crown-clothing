02f096681408e5e16db26eef5e45d7ab3a518bce
Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>
2024-01-27 20:45:03 -0600

    adding header and styling
    Signed-off-by: Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() { // exact without = means true i.e. exact <=> exact={true}
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />  
         <Route exact path='/shop' component={ShopPage} />  
