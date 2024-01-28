3a5495995fb86f96a61cb7bbc026fea6299c3f6a
Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>
2024-01-27 20:24:26 -0600

    Adding collection preview component, collection item component and a shop page
    Signed-off-by: Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import './App.css';
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-);
 
 function App() { // exact without = means true i.e. exact <=> exact={true}
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />  
-        <Route exact path='/hats' component={HatsPage} />  
+        <Route exact path='/shop' component={ShopPage} />  
       </Switch>
     </div>
   );
