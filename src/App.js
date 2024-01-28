6df2786e6339c34eb2aa052126fc9faa9ab1b225
Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>
2024-01-28 00:08:31 -0600

    Signed-off-by: Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() { // exact without = means true i.e. exact <=> exact={true}
@@ -13,6 +14,7 @@ function App() { // exact without = means true i.e. exact <=> exact={true}
       <Switch>
         <Route exact path='/' component={HomePage} />  
         <Route exact path='/shop' component={ShopPage} />  
+        <Route exact path='/signin' component={SignInAndSignUpPage} />  
       </Switch>
     </div>
   );
