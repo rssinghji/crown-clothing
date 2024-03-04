66e2c3a8c6e3161e7c3a083f69d3b3999dccfcbf
rssinghji <rssinghji@gmail.com>
2024-03-03 23:22:46 -0600

    Using structured selector from reselect
    Signed-off-by: rssinghji <rssinghji@gmail.com>

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,9 @@ import { auth } from "../../firebase/firebase.utils";
 import { connect } from 'react-redux';
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { createStructuredSelector } from "reselect";
+import { selectCartHidden  } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser  } from "../../redux/user/user.selectors";
 
 const Header = ({currentUser, hidden}) => (
     <div className="header">
@@ -32,10 +35,21 @@ const Header = ({currentUser, hidden}) => (
     </div>
 );
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser,
-    hidden: state.cart.hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
+
+// const mapStateToProps = (state) => ({
+//     currentUser: selectCurrentUser(state),
+//     hidden: selectCartHidden(state)
+// });  // way of selecting without structured selector
+
+// Replaced with selectors
+// const mapStateToProps = (state) => ({
+//     currentUser: state.user.currentUser,
+//     hidden: state.cart.hidden
+// });
 
 // Above can also be written as:
 // const mapStateToProps = ({user: {currentUser}, cart: { hidden } }) => ({
