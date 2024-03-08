47fa28bd38c7c6c01c0ca7d6af03d32793e6b9a4
rssinghji <rssinghji@gmail.com>
2024-03-07 22:46:44 -0600

    Moving collections and static data to redux
    Signed-off-by: rssinghji <rssinghji@gmail.com>

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 import CollectionPreview from '../../components/preview-collections/collection-preview.component';
-import SHOP_DATA from "./shop.data";
+import  { connect } from 'react-redux';
+import { selectCollections }  from '../../redux/shop/shop.selectors';
+import { createStructuredSelector } from "reselect";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
+const ShopPage = ({collections}) => (
+    <div className="shop-page">
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
         }
-    }
+    </div>
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
+
+export default connect(mapStateToProps)(ShopPage);
