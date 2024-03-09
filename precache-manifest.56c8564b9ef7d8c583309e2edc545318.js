1e42a6e88c0a2aef53866f7ea99fc674d6637814
rssinghji <rssinghji@gmail.com>
2024-03-08 23:29:19 -0600

    Updates

---
+++ precache-manifest.56c8564b9ef7d8c583309e2edc545318.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb67a1a83e5bdc3bb71ff4be766ccb4",
+    "url": "/crown-clothing/home/index.html"
+  },
+  {
+    "revision": "417c131a211a41c75d78",
+    "url": "/crown-clothing/home/static/css/main.e6e646e0.chunk.css"
+  },
+  {
+    "revision": "0c98daddf6d5e8beef35",
+    "url": "/crown-clothing/home/static/js/2.3c4c4b75.chunk.js"
+  },
+  {
+    "revision": "7666c07552f4204c1cc97e847e139bc8",
+    "url": "/crown-clothing/home/static/js/2.3c4c4b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417c131a211a41c75d78",
+    "url": "/crown-clothing/home/static/js/main.b14244a6.chunk.js"
+  },
+  {
+    "revision": "8114a809b10d0ab2d368",
+    "url": "/crown-clothing/home/static/js/runtime-main.7c77f263.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/home/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crown-clothing/home/static/media/shopping-bag.30f07071.svg"
+  }
+]);
