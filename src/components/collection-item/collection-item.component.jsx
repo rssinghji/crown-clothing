b6267b4cf3c9f092edd2abc425bbac22d13f9d90
rssinghji <rssinghji@gmail.com>
2024-03-11 23:11:03 -0500

    Adding some styled components as examples
    Signed-off-by: rssinghji <rssinghji@gmail.com>

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,27 +1,46 @@
 import React from "react";
-import './collection-item.styles.scss';
-import CustomButton from "../custom-button/custom-button.component";
+// import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
 import  { addItem } from '../../redux/cart/cart.actions';
+import {
+    CollectionItemContainer,
+    CollectionFooterContainer,
+    AddButton,
+    BackgroundImage,
+    NameContainer,
+    PriceContainer
+  } from './collection-item.styles';
 
 const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
-    return ( // implies we're rendering multiple javascripts form this funciton
-        <div className="collection-item">
-            <div className="image" 
-                style={{
-                    backgroundImage: `url(${imageUrl})`                
-                }} />
+    // return ( // implies we're rendering multiple javascripts form this funciton
+    //     <div className="collection-item">
+    //         <div className="image" 
+    //             style={{
+    //                 backgroundImage: `url(${imageUrl})`                
+    //             }} />
                 
-            <div className="collection-footer" > 
-                <span className="name">{name}</span>
-                <span className="price">{price}</span>
-            </div>
-            <CustomButton onClick={() => addItem(item)} inverted> 
-                Add To Cart
-            </CustomButton>
-        </div>
-    )
+    //         <div className="collection-footer" > 
+    //             <span className="name">{name}</span>
+    //             <span className="price">{price}</span>
+    //         </div>
+    //         <CustomButton onClick={() => addItem(item)} inverted> 
+    //             Add To Cart
+    //         </CustomButton>
+    //     </div>
+    // )
+    return (
+        <CollectionItemContainer>
+            <BackgroundImage className='image' imageUrl={imageUrl} />
+            <CollectionFooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </CollectionFooterContainer>
+            <AddButton onClick={() => addItem(item)} inverted>
+                Add to cart
+            </AddButton>
+        </CollectionItemContainer>
+    );
 };
 
 const mapDispatchToProps = (dispatch) =>({
