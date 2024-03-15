b9817971a9496ce266ac79e0a7d3a5dbd0119065
rssinghji <rssinghji@gmail.com>
2024-03-15 01:47:56 -0500

    Signed-off-by: rssinghji <rssinghji@gmail.com>

--- src/components/preview-collections/collection-preview.component.jsx
+++ src/components/preview-collections/collection-preview.component.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import './collection-preview.styles.scss';
 import CollectionItem from "../collection-item/collection-item.component";
+import { Link } from "react-router-dom";
 
-const CollectionPreview = ({title, items}) => (
-    <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <div  className="preview">
-            { items
-                .filter((item, index) => index < 4)
-                .map((item) => (
-                    <CollectionItem key={item.id} item={item} />
-                ))
-            }
+const CollectionPreview = ({title, items}) => {
+    const redirectPath = "/shop/"+title.toLowerCase()
+    return (
+        <div className="collection-preview">
+            <Link className="title" to={redirectPath} >{title.toUpperCase()}</Link>
+            <div  className="preview">
+                { items
+                    .filter((item, index) => index < 4)
+                    .map((item) => (
+                        <CollectionItem key={item.id} item={item} />
+                    ))
+                }
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default CollectionPreview;
