0c49efb804e24d13034e379b9a46391068bb9a13
rssinghji <rssinghji@gmail.com>
2024-03-21 22:20:57 -0500

    Adding redux-thunk to move async fetching to redux
    Signed-off-by: rssinghji <rssinghji@gmail.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { persistStore } from 'redux-persist';
-// import logger from  'redux-logger';
+import logger from  'redux-logger';
 import rootReducer from "./root-reducer";
+import {thunk} from 'redux-thunk';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV ===  "development") {
-    // middlewares.push(logger);
+    middlewares.push(logger);
 }
 
 export const store  = createStore(rootReducer, applyMiddleware(...middlewares));
